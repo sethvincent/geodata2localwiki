8992f8079d2958c50560d8bd130b1031728585a3
sethvincent <sethvincent@gmail.com>
2013-04-07 16:17:01 -0700

    add gitignore

--- exp.rb
+++
@@ -1,9 +0,0 @@
-require 'localwiki_client'
-require 'json'
-
-@wiki = LocalwikiClient.new 'seattlewiki.net', 'sethvincent', '0e6580eb57932a358a84977d1ca1fe4343cda85b'
-
-pagename = 'testing map creation 7'
-@map = @wiki.fetch(:map, 'Capitol_Hill')
-
-puts @map.geom.to_json
